ec65bfc4b41ffc7ca27f4435e9939b4d05b95fb0
Gourav sahu <codeholic.gourav@gmail.com>
2023-09-23 18:16:02 +0530

    create organization

--- organization/controller.py
+++ organization/controller.py
@@ -22,58 +22,57 @@ def all_organizations(limit: int, offset: int, db: Session):
 
 
 def create_organization(data: schema.CreateOrganization, db: Session, authToken: frontendModel.FrontendToken):
-    # exiting_name = db.query(model.Organization).filter(model.Organization==data.org_name).first()
-    # if exiting_name:
-    #     CustomValidations.customError(
-    #         type="existing",
-    #         loc="org_name",
-    #         msg="Organization name already exist.",
-    #         inp=data.org_name,
-    #         ctx={"org_name": "unique"}
-    #     )
+    exiting_name = db.query(model.Organization).filter(model.Organization==data.org_name).first()
+    if exiting_name:
+        CustomValidations.customError(
+            type="existing",
+            loc="org_name",
+            msg="Organization name already exist.",
+            inp=data.org_name,
+            ctx={"org_name": "unique"}
+        )
 
-    # if data.registration_type not in model.Organization.allowed_registration:
-    #     CustomValidations.customError(
-    #         type="invalid",
-    #         loc="registration_type",
-    #         msg="Allowed values are 'open', 'approval_required', 'admin_only'",
-    #         inp=data.registration_type,
-    #         ctx={"registration_type": "valid"}
-    #     )
+    if data.registration_type not in model.Organization.allowed_registration:
+        CustomValidations.customError(
+            type="invalid",
+            loc="registration_type",
+            msg="Allowed values are 'open', 'approval_required', 'admin_only'",
+            inp=data.registration_type,
+            ctx={"registration_type": "valid"}
+        )
 
-    # try:
-    #     # Create an instance of OAuth 2.0 credentials using the dictionary
-    #     creds = Credentials.from_authorized_user_info(data.gtoken)
-    # except Exception as e:
-    #     CustomValidations.customError(
-    #         type="Invalid",
-    #         loc="gtoken",
-    #         msg="Not valid google token.",
-    #         inp=str(data.gtoken),
-    #         ctx={"gtoken": "valid"}
-    #     )
+    try:
+        # Create an instance of OAuth 2.0 credentials using the dictionary
+        creds = Credentials.from_authorized_user_info(data.gtoken)
+    except Exception as e:
+        CustomValidations.customError(
+            type="Invalid",
+            loc="gtoken",
+            msg="Not valid google token.",
+            inp=str(data.gtoken),
+            ctx={"gtoken": "valid"}
+        )
 
-    # if not creds or not creds.valid:
-    #     CustomValidations.customError(
-    #         type="Invalid",
-    #         loc="gtoken",
-    #         msg="Not valid google token.",
-    #         inp=str(data.gtoken),
-    #         ctx={"gtoken": "valid"}
-    #     )
+    if not creds or not creds.valid:
+        CustomValidations.customError(
+            type="Invalid",
+            loc="gtoken",
+            msg="Not valid google token.",
+            inp=str(data.gtoken),
+            ctx={"gtoken": "valid"}
+        )
     
-    # organization = model.Organization(
-    #     orguid = generate_uuid(data.org_name),
-    #     org_name = data.org_name,
-    #     admin_id = authToken.user_id,
-    #     gtoken = json.dumps(data.gtoken),
-    #     registration_type = data.registration_type
-    # )
+    organization = model.Organization(
+        orguid = generate_uuid(data.org_name),
+        org_name = data.org_name,
+        admin_id = authToken.user_id,
+        gtoken = json.dumps(data.gtoken),
+        registration_type = data.registration_type
+    )
 
-    # db.add(organization)
-    # db.commit()
-    # db.refresh(organization)
+    db.add(organization)
+    db.commit()
+    db.refresh(organization)
 
-    # return organization
-    return "success"
+    return organization
 
